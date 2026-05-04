0a30ad340f0843922d7719aa83204519a9113496
Kartik-205 <kvrajput1510@gmail.com>
2026-05-04 10:04:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,16 @@ const historyRoutes = require('./routes/history');
 const app = express();
 
 app.use(helmet());
-app.use(cors({ origin: "https://ai-code-review-iota-sand.vercel.app/" }));
+// app.use(cors({ origin: "https://ai-code-review-iota-sand.vercel.app/" }));
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://ai-code-review-iota-sand.vercel.app'
+  ],
+  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}))
 app.use(express.json({ limit: '50kb' }));
 app.use(morgan('dev'));
 
